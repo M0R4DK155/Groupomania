8cd29f8420a6c3b57ec47375bdf9e60123745279
m0r4dk155 <abou.moumoul@gmail.com>
2021-10-26 22:12:24 +0200

    Ajouts et modifications

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();             // Importation du paquet dotenv pour les variables d'environnement.
 const express = require("express");     // Importation du paquet express.
-const app = express();                  // Création de l'application express.
+const app = express();                  // Création de l'application avec express.
 const cors = require('cors');           // On importe Cors.
 const path = require('path');           // Importation du paquet node "path" qui donne accès au chemin du système de fichier.
 const helmet = require('helmet');       // Importation du paquet helmet.
@@ -32,25 +32,24 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get("/", async function (req, res) {
-    await post.getAllPosts(req, res);
-
-})
-
 // Middleware pour traiter les données (en POST) envoyées (transforme le corps de la requête en objet javascript utilisable).
 app.use(express.json());
+
+app.get("/", async function (req, res) {
+    await post.getAllPosts(req, res);
+})
 
 // Middleware spécifique qui permet de servir le dossier image lors d'une requête spécifique avec l'image.
 app.use('/images', express.static(path.join(__dirname, 'images')));   
 
-// pour cette route la, on utilise le router usersRoutes
+// Pour cette route, on utilise le router usersRoutes
 app.use('/user', userRoute);
 
-// pour cette route la, on utilise le router publicationsRoutes
+// Pour cette route, on utilise le router publicationsRoutes
 app.use('/post', postRoute); 
 
-// pour cette route la, on utilise le router commentsRoutes
-app.use('/post', commentRoute);       
+// Pour cette route, on utilise le router commentsRoutes
+app.use('/comment', commentRoute);       
 
 // On exporte la constante app pour qu'on puisse y accéder depuis les autres fichiers de notre projet, notamment notre serveur Node.
 module.exports = app;
